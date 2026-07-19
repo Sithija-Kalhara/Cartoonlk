dbc1412585e206f40340c41e4011b40737da283b
Sithija <sithijakalhara2@gmail.com>
2026-07-20 03:41:01 +0900

    Auto update at startup: 2026-07-20 03:41:00

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
+dns.setServers(['8.8.8.8', '1.1.1.1']); 
+
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/cartoonLK');
-    console.log('✅ MongoDB Connected');
-  } catch (err) {
-    console.error('❌ MongoDB connection error:', err);
-    process.exit(1);
-  }
+  try {
+    await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/cartoonLK');
+    console.log('✅ MongoDB Connected');
+  } catch (err) {
+    console.error('❌ MongoDB connection error:', err);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
       env: {
         NODE_ENV: "production",
         PORT: 5000,
-        MONGO_URI: "mongodb+srv://sithijakalhara2:Flexy%26Code%405521@cluster0.aldctlp.mongodb.net/cartoonLK?retryWrites=true&w=majority&appName=Cluster0",
+        MONGO_URI: "mongodb://sithijakalhara22:FlexyCode5521@cluster0-shard-00-00.9wn40ci.mongodb.net:27017,cluster0-shard-00-01.9wn40ci.mongodb.net:27017,cluster0-shard-00-02.9wn40ci.mongodb.net:27017/cartoonlk?ssl=true&replicaSet=atlas-9wn40ci-shard-0&authSource=admin&retryWrites=true&w=majority",
         R2_ACCESS_KEY_ID: "486fd77544e43e09054a459b7f4ee50b",
         R2_SECRET_ACCESS_KEY: "323b5759fdb87a224691309ba2930fe9599417996e1847279c2815b81b40a548",
         R2_BUCKET: "cartoonlk-videos",
